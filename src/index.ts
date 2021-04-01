d8bdb60cdd8c3d900e1d858ce46b0e247432ce3d
arlinj <arlindejesusmartinez919@gmail.com>
2021-03-31 22:52:07 -1200

    cuarto  video actualicacion

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import  db from './Firebase/Config';
 
 const usuario = {
-    nombre: 'fernando',
+    nombre: 'maria',
     activo: true,
     fechaNaci: 0
 }
 
-// insert into usuarios ....
+const usuariosRef = db.collection('usuarios');
 
-db.collection('usuarios')
-    .add( usuario )
-    .then(docRef => {
-        console.log(docRef)
-    }
+// insert into usuarios ....
+// db.collection('usuarios')
+//    .add( usuario)
+//    .then( docRef => {
+ //   console.log(docRef)
+//  })
+//  .catch{e => console.log('error', e)}
+
+usuariosRef.doc('https://console.firebase.google.com/project/undefined/firestore/data/Fernando%20/XRYxOuyl8AaRTrpo0eO6')
