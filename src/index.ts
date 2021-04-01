c221a8d726b767980ea03db2003c3a86096b31f8
arlinj <arlindejesusmartinez919@gmail.com>
2021-03-31 22:06:48 -1200

    tercer video insertar data en firestore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import './Firebase/Config';
+import  db from './Firebase/Config';
+
+const usuario = {
+    nombre: 'fernando',
+    activo: true,
+    fechaNaci: 0
+}
+
+// insert into usuarios ....
+
+db.collection('usuarios')
+    .add( usuarios )
